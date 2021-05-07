b27e06dad1c172ce9faee9adccb7f3709c735938
Kouki-Kubota <kubou1115@outlook.jp>
2021-05-08 00:47:00 +0900

    ヘッダーを追加

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <!-- ここにヘッダーをコーディング -->
+    <header>
+      <v-app-bar
+        app
+        dark
+      >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <v-toolbar-title>Vue-News</v-toolbar-title>
+      </v-app-bar>
+      <v-navigation-drawer
+        v-model="drawer"
+        fixed
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group>
+            <v-list-item
+      v-for="(menuItem, index) in menuItems"
+      :key="index"
+    >
+      <v-list-item-title>{{ menuItem.name }}</v-list-item-title>
+    </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </header>
+  </v-app>
+</template>
+ 
+<script>
+import constants from '../common/constants'
+
+export default {
+  data () {
+    return {
+      drawer: false,
+      menuItems: constants.menuItems
+    }
+  }
+}
+</script>
+ 
+<style></style>
